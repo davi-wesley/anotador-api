414a97b4bc45a8f9994b7fd035ba5ae52b0cf087
davi-wesley <daviwesley00023@gmail.com>
2022-07-05 12:29:43 -0300

    primeiro commit com conexão de banco de dados já adicionada

---
+++ src/main/java/com/dvtec/anotador/AnotadorApiApplication.java
@@ -0,0 +1,13 @@
+package com.dvtec.anotador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotadorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotadorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dvtec/anotador/AnotadorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvtec.anotador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotadorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
